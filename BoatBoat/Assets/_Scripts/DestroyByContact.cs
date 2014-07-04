ced94e54da614555cfcaacb1f281214595d2608c
Chunga64 <cjohnston8@gatech.edu>
2014-07-04 02:20:08 -0400

    Added destroying scripts

---
+++ BoatBoat/Assets/_Scripts/DestroyByContact.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Ocean")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		//Add when enemy boat needs to die
+//		if (other.tag == "Player")
+//		{
+//			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+//			gameController.GameOver ();
+//		}
+//		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
