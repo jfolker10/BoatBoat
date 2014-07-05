72a502e2391548af55cff56205c472a5ac2e5931
Chunga64 <Chase@Chases-MacBook-Pro-2.local>
2014-07-05 00:43:31 -0400

    Added 6 "positions" for cannons

--- BoatBoat/Assets/_Scripts/DestroyByContact.cs
+++ BoatBoat/Assets/_Scripts/DestroyByContact.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class DestroyByContact : MonoBehaviour {
 
 	public GameObject explosion;
-	
-	//void OnTriggerEnter(Collider other) 
+
 	void OnCollisionEnter(Collision other)
 	{
 		if (other.collider.tag == "Spout Sector")
@@ -14,12 +13,11 @@ public class DestroyByContact : MonoBehaviour {
 		}
 		Instantiate(explosion, transform.position, transform.rotation);
 		Destroy(gameObject);
-		//Add when enemy boat needs to die
-//		if (other.tag == "Player")
+		//Add when enemy boat needs to die, maybe put on ship.cs
+//		if ((other.collider.tag == "Player") || (other.collider.tag == "Enemy"))
 //		{
-//			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-//			gameController.GameOver ();
+//			call sinking animation
 //		}
-//		Destroy(other.gameObject);
+//		Destroy(other.gameObject, lifeTime);
 	}
 }
